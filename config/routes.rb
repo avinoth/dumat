1baa77f3e38c6b281bfdc0bb43ac406c0f6813bf
Vinoth <a.vinothk@outlook.com>
2015-07-25 14:54:49 +0530

    add bootstrap and material theme

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/home', to: 'static#home'
   get '/about', to: 'static#about'
 
+  get '/auth/github', as: :login
   get 'auth/:provider/callback', to: 'users#create'
   get '/logout', to: 'users#logout', as: :logout
 
